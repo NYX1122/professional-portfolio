51d71a6caf7b6fc1c9e5eab02346ee8a85751f90
BoDee <nyx0hemir@gmail.com>
2021-09-16 22:48:44 -0600

    done with file structure

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <header>
+            <h1 className='name'>BoDee Angus</h1>
+            <nav>
+                
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
