cc626b7505798d62c933d374d565fb3fa4414103
BoDee <nyx0hemir@gmail.com>
2021-09-18 17:38:59 -0600

    footer elements and styling done. linking still needs to be completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
+import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className='app-div'>
       <Header></Header>
-      <About></About>
+      <Portfolio></Portfolio>
+      {/* <About></About> */}
       <Footer></Footer>
     </div>
   );
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Portfolio() {
+    return (
+        <div>test</div>
+    );
+}
+
+export default Portfolio;
