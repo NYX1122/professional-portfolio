0a4ba6ab79e5c94da6e130779e95a58e98a12e49
BoDee <nyx0hemir@gmail.com>
2021-10-20 16:53:29 -0600

    finished contact functionality and icons / page name

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,20 +9,20 @@ function About() {
                     <div className='about-para'>
                         <h3>Background</h3>
                         <p>
-                            <span class={'indent'}></span>To begin I’d just like to say thanks for viewing my portfolio page, I hope you like it :) . I’m a twenty
+                            <span className={'indent'}></span>To begin I’d just like to say thanks for viewing my portfolio page, I hope you like it :) . I’m a twenty
                             year old junior full-stack web developer, trained by the full-stack coding bootcamp offered by the University of Utah. Before I began 
                             my web development career, I studied at Utah State University under a computer science major for two semesters, while working in fast 
                             food and retail. Thus, my adult life has only consisted of education and working in the food and retail service industries so far,
                             with the addition of every project I've worked on in the bootcamp of course.
                             <br/>
                             <br/>
-                            <span class={'indent'}></span>I grew up on a farm in eastern Utah, and attended Uintah High School at the nearby town of Vernal. As
+                            <span className={'indent'}></span>I grew up on a farm in eastern Utah, and attended Uintah High School at the nearby town of Vernal. As
                             a teenager, I always wanted to make my family proud by attending college and obtaining a degree, because my parents and grandparents 
                             never had the opportunity themselves and always wanted me to make something more of myself. This reason, along with my childhood 
                             dream to work in the tech industry, has led me to pursuing a career in web development.
                             <br/>
                             <br/>
-                            <span class={'indent'}></span>And what a journey it’s been! I moved across Utah to the Salt Lake Valley, leaving the place I grew up 
+                            <span className={'indent'}></span>And what a journey it’s been! I moved across Utah to the Salt Lake Valley, leaving the place I grew up 
                             behind in order to broaden my opportunities and kick-start my professional career. It was definitely difficult adjusting to living in 
                             a metropolis when I came from a town with only 1,000 people, but it was worth it in the end, as I’m now a full-stack web developer!
                         </p>
@@ -31,7 +31,7 @@ function About() {
                 <div className='about-row-two'>
                     <div className='about-para'>
                         <h3>Skillset</h3>
-                        <p><span class={'indent'}></span>
+                        <p><span className={'indent'}></span>
                             I am proficient in CSS, HTML, JavaScript, SQL, and JSX, as well as a number of tools and technologies associated with these languages. 
                             My certification in full-stack web development focused on a MERN (Mongoose, Express, React, and Node) type web application. I also 
                             learned a number of best practices and coding paradigms to help make me industry competitive, such as the mobile first development 
@@ -40,7 +40,7 @@ function About() {
                             associated with them. 
                             <br/>
                             <br/>
-                            <span class={'indent'}></span>Along with these Web Development skills I also have high typing speed (69 WPM based on the medium text
+                            <span className={'indent'}></span>Along with these Web Development skills I also have high typing speed (69 WPM based on the medium text
                             test on typingtest.com), proficiency in Microsoft office applications, proficiency in Google drive web applications, and proficiency
                             in using Adobe inDesign. I am a very social person who is known for my extroversion, and as such I have always worked well in group
                             work environments. To add to my workplace strengths, I am an optimistic and enthusiastic person who strives to find positivity
@@ -50,7 +50,7 @@ function About() {
                     </div>
                     <div className='about-para'>
                         <h3>Future Goals</h3>
-                        <p><span class={'indent'}></span>
+                        <p><span className={'indent'}></span>
                             Currently I am hoping to join the web development workforce so that I may finally begin to gain proper work experience and expand my web-dev
                             skills. After I find a job, I also want to continue my secondary education to acheive a bachelors in Computer Science, while minoring in web
                             development. I am very passionate about acheiving the goals I set out for myself, and as such any employer that allows me the opportunity to
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 function Contact() {
+    const [modal, toggleModal] = useState('');
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const inquirerName = e.target[0].value;
+        const company = e.target[1].value;
+        const inquirerEmail = e.target[2].value;
+        const message = e.target[3].value;
+
+        if (inquirerName && company && inquirerEmail && message) {
+            const templateParams = {
+                user_name: inquirerName,
+                company: company,
+                user_email: inquirerEmail,
+                message: message
+            };
+
+            await emailjs.send('service_8foh7wh', 'contact_form', templateParams, 'user_D9Sj4kQlybYrXXUQ34cjK')
+                .then((response) => {
+                    console.log('SUCCESS!', response.status, response.text);
+                    toggleModal('success');
+                }, (err) => {
+                    console.log('FAILED...', err);
+                    toggleModal('error');
+                });
+        } else {
+            toggleModal('validationError');
+            console.log(modal);
+        }
+    }
+
+    const closeModal = () => {
+        toggleModal('');
+        window.location.reload();
+    }
+
+    const closeModalValidation = () => {
+        toggleModal('');
+    }
+
     return (
         <section className='contact'>
             <h2>Contact Me</h2>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div>
                     <label htmlFor='name'>Name:</label>
                     <input type='text' name='name'/>
@@ -23,6 +64,28 @@ function Contact() {
                 </div>
                 <button type='submit'>Submit</button>
             </form>
+            {(modal === 'success') ?
+                <div className='modal'>
+                    <div className='modal-content'>
+                        <h1>BoDee has successfully received your message.</h1>
+                        <button onClick={closeModal}>Understood</button>
+                    </div>
+                </div>
+            : (modal === 'error') ?
+                <div className='modal'>
+                    <div className='modal-content'>
+                        <h1>There was an error processing your message. Please try again or contact BoDee using another method.</h1>
+                        <button onClick={closeModal}>Understood</button>
+                    </div>
+                </div>
+            : (modal === 'validationError') &&
+                <div className='modal'>
+                    <div className='modal-content'>
+                        <h1>There was an error validating your message. Please make sure that all fields are provided.</h1>
+                        <button onClick={closeModalValidation}>Understood</button>
+                    </div>
+                </div>
+            }
         </section>
     )
 };
